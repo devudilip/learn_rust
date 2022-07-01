812b39e687f6c4f4a57ee8fca807d8f95f77d594
DEVARAJ K <devu.dilip@gmail.com>
2022-07-02 00:21:12 +0530

    variables

--- practice_rust/src/main.rs
+++ practice_rust/src/main.rs
@@ -1,8 +1,21 @@
-fn main() {
-    println!("Hello, main function!");
-    another_function();
+
+fn five() -> i32 {
+    5
 }
 
-fn another_function() {
-    println!("Hello, another function!");
+fn main() {
+    let x = five();
+
+    println!("The value of x is: {}", x);
 }
+
+
+// fn main() {
+//     println!("Hello, main function!");
+//     another_function(5, 'r');
+// }
+
+// fn another_function(x: i32, unit_label: char) {
+//     println!("Hello, another function!");
+//     println!("Hello, The value of x : {}-{}", x,unit_label);
+// }

587130cd4c64d7164887f682d303c5094ebb02ce
DEVARAJ K <devu.dilip@gmail.com>
2022-07-05 21:18:34 +0530

    conditions

--- practice_rust/src/main.rs
+++ practice_rust/src/main.rs
@@ -1,13 +1,34 @@
+// Loops
 
-fn five() -> i32 {
-    5
-}
 
-fn main() {
-    let x = five();
 
-    println!("The value of x is: {}", x);
-}
+
+
+
+// condition
+// fn main() {
+//     let number = 6;
+
+//     if number % 4 == 0 {
+//         println!("number is divisible by 4");
+//     } else if number % 3 == 0 {
+//         println!("number is divisible by 3");
+//     } else if number % 2 == 0 {
+//         println!("number is divisible by 2");
+//     } else {
+//         println!("number is not divisible by 4, 3, or 2");
+//     }
+// }
+
+// fn five() -> i32 {
+//     5
+// }
+
+// fn main() {
+//     let x = five();
+
+//     println!("The value of x is: {}", x);
+// }
 
 
 // fn main() {
